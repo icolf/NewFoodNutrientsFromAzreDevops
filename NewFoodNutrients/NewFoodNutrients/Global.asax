26b27a41a5b3fe965b68f46ba7d3eb1982ccc13e
Luis E. Figueroa <icolfigueroa@gmail.com>
2018-04-07 17:56:13 -0500

    Initial commit.

---
+++ NewFoodNutrients/NewFoodNutrients/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewFoodNutrients.MvcApplication" Language="C#" %>
---
+++ NewFoodNutrients/NewFoodNutrients/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewFoodNutrients.Startup))]
+namespace NewFoodNutrients
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
