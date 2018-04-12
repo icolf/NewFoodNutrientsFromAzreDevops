4b6716e1ef8f9e608c5187acae860bbf1ddc27a0
Luis E. Figueroa <icolfigueroa@gmail.com>
2018-04-11 19:28:51 -0500

    Create RecipesController, Create view, and chabges to nav-bar.

---
+++ NewFoodNutrients/NewFoodNutrients/Controllers/RecipesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewFoodNutrients.Controllers
+{
+    public class RecipesController : Controller
+    {
+        // GET: Recipes
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
