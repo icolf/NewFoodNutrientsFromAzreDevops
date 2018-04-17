4d479b88b4030665e8fb13e86758ea9c083a47e9
Luis E. Figueroa <icolfigueroa@gmail.com>
2018-04-16 20:51:23 -0500

    Add PlateViewModel.cs and Populate FoodType table

--- NewFoodNutrients/NewFoodNutrients/Controllers/RecipesController.cs
+++ NewFoodNutrients/NewFoodNutrients/Controllers/RecipesController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NewFoodNutrients.Models;
+using NewFoodNutrients.ViewModels;
 
 namespace NewFoodNutrients.Controllers
 {
     public class RecipesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public RecipesController( )
+        {
+            _context = new ApplicationDbContext();
+
+        }
         // GET: Recipes
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new PlateViewModel();
+            viewModel.FoodTypes = _context.FoodTypes.ToList();
+            return View(viewModel);
         }
     }
 }
---
+++ NewFoodNutrients/NewFoodNutrients/ViewModels/PlateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NewFoodNutrients.Models;
+
+namespace NewFoodNutrients.ViewModels
+{
+    public class PlateViewModel
+    {
+        public int FoodType { get; set; }
+        public List<FoodType> FoodTypes { get; set; }
+    }
+}
